146ba7d453faa2dd85132d90f47bbf7028710e31
MilkTablee <119823959+MilkTablee@users.noreply.github.com>
2024-07-12 03:07:55 +0100

    Finished implementing API endpoint. Connected to database, frontend now reflects added transactions as fetched from database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
@@ -8,8 +11,22 @@ app.get('/api/test', (req, res) => {
   res.json('test ok');
 });
 
-app.post('/api/transaction', (req, res) => {
-  res.json(req.body);
+app.post('/api/transaction', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const { price, name, description, datetime } = req.body; // grab JSON information from request body
+  const transaction = await Transaction.create({
+    price,
+    name,
+    description,
+    datetime
+  });
+  res.json(transaction);
+});
+
+app.get('/api/transactions', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await Transaction.find();
+  res.json(transactions);
 });
 
 app.listen(4000);
--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const [name, setName] = useState('');
   const [datetime, setDatetime] = useState('');
   const [description, setDescription] = useState('');
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL+'/transactions';
+    const response = await fetch(url);
+    return await response.json();
+  }
+
   function addNewTransaction(e) {
     e.preventDefault();
     const url = process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split(' ')[0];
     fetch(url, {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({ name, description, datetime })
+      body: JSON.stringify({
+        price,
+        name:name.substring(price.length+1),
+        description,
+        datetime,
+      })
     }).then(response => {
       response.json().then(json => {
-        console.log('result', json);
+        setName('');
+        setDatetime('');
+        setDescription('');
+        setTransactions([...transactions, json]);
+        //console.log('result', json);
       });
     });
   }
+
+  // TODO: IMPLEMENT A FUNCTION TO HANDLE DELETION OF TRANSACTION
+  // TODO IMPLEMENT A FUNCTION TO HANDLE EDITING A TRANSACTION
+
+  // BALANCE FORMATTING LOGIC
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
+
+  balance = balance.toFixed(2);
+  const fraction = balance.split('.')[1];
+  balance = balance.split('.')[0];
+
   return (
+    // TODO: IMPLEMENT SEPARATE INPUT FIELD FOR PRICE
+    // TODO: IMPLEMENT TOGGLE FOR INCOME/EXPENSE
     <main>
-      <h1>£400<span>.00</span></h1>
+      <h1>£{balance}<span>{fraction}</span></h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic">
           <input type="text"
                  value={name}
                  onChange={e => setName(e.target.value)}
-                 placeholder={'+20 new dildo'}/>
+                 placeholder={'+20 item sold'}/>
           <input type="datetime-local"
                  value={datetime}
                  onChange={e => setDatetime(e.target.value)}/>
@@ -40,37 +78,34 @@ function App() {
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Dildo</div>
-            <div className="description">Can't go wrong with a new dildo</div>
-          </div>
-            <div className="right">
-              <div className="price red">-£20</div>
-              <div className="datetime">2023-12-18 15:45</div>
+      {transactions.length > 0 && transactions.map(transaction => {
+        // DATETIME FORMATTING LOGIC
+        const dateObject = new Date(transaction.datetime);
+        const formattedDateTime = dateObject.toLocaleString('en-GB', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false
+        });
+        return (
+          <div className="transaction" key={transaction._id}>
+            <div className="left">
+              <div className="name">{transaction.name}</div>
+              <div className="description">{transaction.description}</div>
             </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Feet pics</div>
-            <div className="description">Crusty ahh feet be selling</div>
-          </div>
             <div className="right">
-              <div className="price green">£150</div>
-              <div className="datetime">2023-12-18 15:45</div>
+              <div className={"price " + (transaction.price < 0 ? "red" : "green")}>
+                {transaction.price}
+              </div>
+              <div className="datetime">{formattedDateTime}</div>
             </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Penis enlargement surgery</div>
-            <div className="description">Can't go wrong with a new dildo</div>
           </div>
-            <div className="right">
-              <div className="price red">-£1350</div>
-              <div className="datetime">2023-12-18 15:45</div>
-            </div>
-        </div>
-      </div>
+        )
+      })}
+    </div>
     </main>
   );
 }
